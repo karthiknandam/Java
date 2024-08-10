5fde5346c204641c3f60481fc7902dc621f573ee
Karthik Nandam <140495326+karthiknandam@users.noreply.github.com>
2024-08-10 23:21:03 +0530

    Add files via upload

--- Day - 13/EnumCollect.java
+++ Day - 13/EnumCollect.java
@@ -5,8 +5,23 @@ public class EnumCollect {
     public static void main(String[] args) {
 
         for(int i = 0 ; i<10 ; i++){
-            DaysOfWeeks x = getVal();
-            System.out.println(x.name() +" "+ x.ordinal());
+           getDays(getVal());
+        }
+        double total = 0;
+        for(DaysOfWeeks day : DaysOfWeeks.values()){
+
+            total+= day.getPay();
+            System.out.println(total);
+        }
+
+    }
+    public static void getDays (DaysOfWeeks day){
+        int val = day.ordinal()+1;
+
+        switch (day){
+            case SUN -> System.out.println(day.name().charAt(0)+day.name().substring(1).toLowerCase() + ":" + " Is holiday Pay is : " + (day.getPay()));
+            case SAT -> System.out.println(day.name().charAt(0)+day.name().substring(1).toLowerCase() + ":" + " First holiday Pay is : " +(day.getPay()));
+            default -> System.out.println("Week day no : "+ val +" "+ day.name() + " Pay is : " +(day.getPay()));
         }
 
     }
@@ -17,5 +32,11 @@ public class EnumCollect {
     }
 }
 enum DaysOfWeeks {
-    SUN,MON,TUE,WED,THU,FRI,SAT
+    SUN,MON,TUE,WED,THU,FRI,SAT;
+    public double getPay(){
+        return switch (this){
+            case SUN , SAT -> 0;
+            default -> 15.5;
+        };
+    }
 }
